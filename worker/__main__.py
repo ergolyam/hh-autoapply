70c3fa8f82a5cd51d371fd49d04704b8875cff60
ergolyam <grisha765@tuta.io>
2025-10-21 19:01:10 +0300

    [u] test implementation

--- worker/__main__.py
+++ worker/__main__.py
@@ -1,18 +1,18 @@
-import os, sys
+import os, sys, asyncio
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from worker.config.config import Config
-from worker.config.config_yaml import load_config, YamlConfig
+from worker.config.config_yaml import load_config
 
 from worker.config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 logging.info(f"Script initialization, logging level: {Config.log_level}")
 
-def main():
-    config = load_config()
-    cfg = YamlConfig()
-    logging.debug(cfg['settings']['oauth_token'])
+async def main():
+    from worker.api.get_resume import get_my_resumes
+    load_config()
+    print(await get_my_resumes())
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
