a09cad13bb140d6ea3b95aff446c8055955449fe
ergolyam <grisha765@tuta.io>
2025-11-05 18:38:07 +0300

    [u] simple json

--- worker/__main__.py
+++ worker/__main__.py
@@ -4,10 +4,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from worker.config.config import Config
 from worker.config.config_yaml import load_config
 
+from worker.api.get_resume import get_my_resumes
+
 async def main():
-    from worker.api.get_resume import get_my_resumes
-    load_config()
-    print(await get_my_resumes())
+    resumes = await get_my_resumes()
+    resumes_dict = {}
+    for idx, r in enumerate(resumes.get("resumes"), start=1):
+        resumes_dict[idx] = {
+            "resume_id": r.get("id", ""),
+            "title": r.get("title", "")
+        }
+    print(resumes_dict)
 
 if __name__ == '__main__':
     asyncio.run(main())
