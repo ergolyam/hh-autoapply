9f13622f8c612eacac3ae9c5350824e10b243263
ergolyam <grisha765@tuta.io>
2025-11-07 01:14:31 +0300

    [+] add extra args

--- worker/api/base.py
+++ worker/api/base.py
@@ -1,15 +1,25 @@
 from worker.core.helpers import Common
 from worker.config.config import Config
 
-async def request(path: str):
+async def request(path: str, extra_headers: dict = {}, params: dict = {}):
     headers = {
         "Authorization": f"Bearer {Common.cfg['settings']['oauth_token']}",
         "User-Agent": f"{Config.app_name}/1.0 ({Config.app_email})",
     }
+    if extra_headers:
+        headers.update(extra_headers)
     
     url = f"https://{Config.hh_api_domain}/{path}"
+
+    kargs = {
+        "url": url,
+        "headers": headers
+    }
+
+    if params:
+        kargs['params'] = params
     
-    response = await Common.http.get(url, headers=headers)
+    response = await Common.http.get(**kargs)
     if response.status_code == 200:
         data = response.json()
         return {"ok": True, "data": data}
