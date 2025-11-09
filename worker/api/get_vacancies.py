fa4eaafaef76d1a78857d80f6519f497e305b46a
ergolyam <grisha765@tuta.io>
2025-11-09 22:31:43 +0300

    [f] adaptation to none

--- worker/api/get_vacancies.py
+++ worker/api/get_vacancies.py
@@ -11,18 +11,23 @@ async def vacancy_detals(id: int):
         raise Exception(f'Error {resp.get('status_code', None)}: {resp.get('details', None)}')
 
 
+def drop_none(d: dict) -> dict:
+    return {k: v for k, v in d.items() if v is not None}
+
+
 async def vacancies_request(resume_id: str, page: int = 0):
+    params = drop_none({
+        'page': page,
+        'per_page': 10,
+        'text': Common.cfg['vacancies']['text'],
+        'experience': Common.cfg['vacancies']['experience'],
+        'employment': Common.cfg['vacancies']['employment'],
+        'schedule': Common.cfg['vacancies']['schedule'],
+        'salary': Common.cfg['vacancies']['salary']
+    })
     resp = await request(
         path=f'resumes/{resume_id}/similar_vacancies',
-        params={
-            'page': page,
-            'per_page': 10,
-            'text': Common.cfg['vacancies']['text'],
-            'experience': Common.cfg['vacancies']['experience'],
-            'employment': Common.cfg['vacancies']['employment'],
-            'schedule': Common.cfg['vacancies']['schedule'],
-            'salary': Common.cfg['vacancies']['salary']
-        }
+        params=params
     )
     if resp.get('ok', False):
         data = resp.get('data', {})
