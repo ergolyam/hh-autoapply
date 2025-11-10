525cde20fba3b5396af5ff96da6ff1f8a33cb224
ergolyam <grisha765@tuta.io>
2025-11-10 18:13:18 +0300

    [r] refactor on match case & [+] add help command

--- worker/__main__.py
+++ worker/__main__.py
@@ -7,30 +7,45 @@ from worker.core.helpers import Common
 
 from worker.api.get_resume import resume_request
 from worker.api.get_vacancies import vacancies_request
+from worker.api.get_handbook import handbook_request
 
 async def main():
     argv = sys.argv[1:]
-    resumes = await resume_request()
-    vacancies = await vacancies_request(
-        resume_id=Common.cfg['settings']['resume_id'],
-        page=0
-    )
-    if not argv:
-        print(resumes)
-        print(json.dumps(vacancies, indent=4, ensure_ascii=False))
-        return
-    try:
-        idx = int(argv[1])
-    except ValueError:
-        print(f'The index must be a number, not "{argv[1]}"')
-        return
-    if len(argv) == 2 and argv[0] == '--set':
-        value = resumes.get(idx)
-        assert value
-        value_id = value.get('resume_id')
-        Common.cfg['settings']['resume_id'] = value_id
-        Common.cfg.save()
-        print(f'Set resume id: {value_id}')
+    match argv:
+        case ['--help', topic]:
+            print(await handbook_request(topic))
+        case ['--help']:
+            print('''Usage:
+script.py --resume <index> - display all resumes or set the active resume by index
+script.py --help [ experience, employment, schedule ] - display this message or help on the topic
+            ''')
+        case ['--resume', idx]:
+            resumes = await resume_request()
+            try:
+                idx = int(idx)
+            except ValueError:
+                print(f'Invalid index: {idx}')
+                return
+            value = resumes.get(idx)
+            if not value:
+                print(f'Resume with index {idx} not found')
+                return
+            assert value
+            value_id = value.get('resume_id')
+            Common.cfg['settings']['resume_id'] = value_id
+            Common.cfg.save()
+            print(f'Set resume id: {value_id}')
+        case ['--resume']:
+            resumes = await resume_request()
+            print(resumes)
+        case [] | _ if not argv:
+            vacancies = await vacancies_request(
+                resume_id=Common.cfg['settings']['resume_id'],
+                page=0
+            )
+            print(json.dumps(vacancies, indent=4, ensure_ascii=False))
+        case _:
+            print('Unknown command. Use --help')
 
 if __name__ == '__main__':
     asyncio.run(main())
