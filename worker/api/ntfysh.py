7138a1a81e9de7d07ef3152162cd8cf73b801a01
ergolyam <grisha765@tuta.io>
2025-11-18 23:56:41 +0300

    [+] add ntfysh func

---
+++ worker/api/ntfysh.py
@@ -0,0 +1,34 @@
+from worker.core.helpers import Common
+from worker.config.config import Config
+
+async def send_notify(text: str, title: str = '', click: str = ''):
+    url = f'{Config.ntfy_url}/{Config.ntfy_topic}'
+
+    kargs: dict = {
+        'url': url,
+        'content': text.encode(encoding='utf-8')
+    }
+
+    if title or click:
+        kargs.setdefault('headers', {})
+
+    if title:
+        kargs['headers']['Title'] = title.encode(encoding='utf-8')
+
+    if click:
+        kargs['headers']['Click'] = click
+
+    response = await Common.http.post(**kargs)
+    if response.status_code == 200:
+        data = response.json()
+        return {'ok': True, 'data': data}
+
+    try:
+        err = response.json()
+        details = err.get('description') or err.get('error') or err
+    except Exception:
+        details = response.text
+    return {'ok': False, 'status_code': response.status_code, 'details': details}
+
+if __name__ == '__main__':
+    raise RuntimeError('This module should be run only via main.py')
