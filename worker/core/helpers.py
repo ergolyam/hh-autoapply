6c2b965e09127870ca2d3e818df1d59b69aa3649
ergolyam <grisha765@tuta.io>
2025-12-13 00:00:27 +0300

    [r] screenshot as bytes

--- worker/core/helpers.py
+++ worker/core/helpers.py
@@ -1,10 +1,13 @@
-import os
+import httpx, io
 
-async def take_screenshot(page, file_name):
-    print(f'Taking screenshot: {file_name}')
-    await page.screenshot(path=file_name)
-    print(f'Screenshot saved as {file_name}')
-    os.system(f'imv {file_name}')
+async def take_screenshot(page, *, full_page: bool = False, img_type: str = "png") -> io.BytesIO:
+    data: bytes = await page.screenshot(full_page=full_page, type=img_type)
+    return io.BytesIO(data)
+
+class Common():
+    client_timeout = httpx.Timeout(30.0)
+    http = httpx.AsyncClient(timeout=client_timeout)
+
 
 if __name__ == '__main__':
     raise RuntimeError('This module should be run only via main.py')
