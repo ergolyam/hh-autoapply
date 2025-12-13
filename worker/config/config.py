3251cc628854388b67dff987837f8c52f4c24609
ergolyam <grisha765@tuta.io>
2025-12-13 03:39:26 +0300

    [+] new params

--- worker/config/config.py
+++ worker/config/config.py
@@ -5,10 +5,12 @@ class Config:
     state_path: str = './'
     hh_domain: str = 'hh.ru'
     email: str = 'example@domain.com'
+    search_text: str = 'Python Developer'
     model_name: str = ''
     api_key: list = []
     openai_base_url: str = ''
     retries: int = 10
+    filter_phrase: str = 'Accept job offers related to Python development.'
     ntfy_url: str = 'https://ntfy.sh'
     ntfy_topic: str = 'ergolyam'
     db_path: str = 'data.db'
